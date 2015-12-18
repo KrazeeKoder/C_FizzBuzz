9464a4f4631cc51ff561ef80139384421a8ce722
KrazeeKoder <KrazeeKoder@users.noreply.github.com>
2015-12-18 01:36:40 -0500

    Update main.m
    Deleted unnecessary comments

--- C_FizzBuzz/main.m
+++ C_FizzBuzz/main.m
@@ -6,15 +6,6 @@
 //  Copyright © 2015 Anthony Tulai. All rights reserved.
 //
 
-//#import <Foundation/Foundation.h>
-
-//int main(int argc, const char * argv[]) {
-//    @autoreleasepool {
-//        // insert code here...
-//        NSLog(@"Hello, World!");
-//    }
-//    return 0;
-//}
 #include <stdio.h>
 
 int main()
